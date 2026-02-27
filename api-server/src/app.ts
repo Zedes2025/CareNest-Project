02534b5434bdecd2beda2165673b68e1a22de8c4
Zeinab <zeinabsadjadi@gmail.com>
2026-02-27 12:22:47 +0100

    api server structured

---
+++ api-server/src/app.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import "#db";
+
+import { errorHandler } from "#middlewares";
+const app = express();
+const port = process.env.PORT || 1000;
+app.use((req, res, next) => {
+  next();
+});
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use("*splat", (req, res) => {
+  throw new Error("Not found", { cause: { status: 404 } });
+});
+
+app.use(errorHandler);
+
+app.listen(port, () =>
+  console.log(`Server is running on port http://localhost:${port}`),
+);

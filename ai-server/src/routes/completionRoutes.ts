4cbd8d314295d4186cf92ecb2ba3e7e86a2b1713
Zeinab <zeinabsadjadi@gmail.com>
2026-03-02 12:22:25 +0100

    ai server finally conncted and ai answersgit add .!

--- ai-server/src/routes/completionRoutes.ts
+++ ai-server/src/routes/completionRoutes.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express';
 import { validateBody } from '#middleware';
 import { promptSchema } from '#schemas';
-
+import { createInMemoryChat } from '#controllers';
 const completionRoutes = Router();
 
-completionRoutes.post('/in-memory-chat', validateBody(promptSchema));
+completionRoutes.post('/chat', validateBody(promptSchema), createInMemoryChat);
 
 export default completionRoutes;

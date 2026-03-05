f1a6720d3688ce97896339e95de9454b093bf22c
Zeinab <zeinabsadjadi@gmail.com>
2026-03-05 12:52:31 +0100

    ui card added for notification

--- web-server/src/pages/ContactPage.tsx
+++ web-server/src/pages/ContactPage.tsx
@@ -3,8 +3,31 @@
 export const ContactPage = () => {
   return (
     <div className="container mx-auto px-4 py-10">
-      <h1 className="text-2xl font-semibold">Contacts</h1>
-      <p className="mt-2 opacity-70">Placeholder page.</p>
+      <h1 className="text-2xl font-semibold">Notifications</h1>
+      <h2 className="text-lg font-semibold mt-4 mb-4">Pending requests</h2>
+
+      <div className="card w-96 bg-base-100 card-xs shadow-sm border">
+        <div className="card-body  ">
+          <h2 className="card-title justify-center">Username</h2>
+          <div className="flex row">
+            <div className="avatar">
+              <div className="w-24 rounded-full">
+                <img src="https://img.daisyui.com/images/profile/demo/yellingcat@192.webp" />
+              </div>
+            </div>
+
+            <p>username wants to connect with you!</p>
+          </div>
+          <div className="justify-end card-actions">
+            <button className="btn btn-sm btn-neutral rounded-xl">
+              Accept
+            </button>
+            <button className="btn btn-sm  rounded-xl">Decline</button>
+          </div>
+        </div>
+      </div>
+
+      <h2 className="text-lg font-semibold mt-4 mb-4">Previous Responses</h2>
     </div>
   );
 };

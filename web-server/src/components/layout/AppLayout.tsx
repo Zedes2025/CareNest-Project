e4258a9ad6594c0eefd48174ef22142138f00a37
Konstantin Schimitzek <k.schimitzek@web.de>
2026-03-12 15:55:02 +0100

    reworked Frontend on ContactPage

--- web-server/src/components/layout/AppLayout.tsx
+++ web-server/src/components/layout/AppLayout.tsx
@@ -1,29 +1,67 @@
+import { AuthProvider } from "../../contexts/AuthContext";
 import { Footer } from "./Footer";
 import { Header } from "./Header";
 import { Outlet, useNavigation } from "react-router";
-import { ChatBtn } from "../ui/aiChatBtn";
 
 export const AppLayout = () => {
   const navigation = useNavigation();
   const isLoading = navigation.state === "loading";
 
   return (
-    <>
-      <Header />
-      <div className="min-h-dvh">
-        {isLoading && (
-          <div className="container">
-            <span className="loading loading-ring loading-xs"></span>
-            <span className="loading loading-ring loading-sm"></span>
-            <span className="loading loading-ring loading-md"></span>
-            <span className="loading loading-ring loading-lg"></span>
-            <span className="loading loading-ring loading-xl"></span>
+    <AuthProvider>
+      <div className="min-h-dvh flex flex-col">
+        <Header />
+
+        {/* make main a flex container */}
+        <main className="flex-1 min-h-0 flex flex-col">
+          {isLoading && (
+            <div className="container py-3">
+              <span className="loading loading-ring loading-xs"></span>
+              <span className="loading loading-ring loading-sm"></span>
+              <span className="loading loading-ring loading-md"></span>
+              <span className="loading loading-ring loading-lg"></span>
+              <span className="loading loading-ring loading-xl"></span>
+            </div>
+          )}
+
+          {/* outlet area needs to be flex-1 */}
+          <div className="flex-1 min-h-0">
+            <Outlet />
           </div>
-        )}
-        <Outlet />
+        </main>
+
+        <Footer />
       </div>
-      <ChatBtn />
-      <Footer />
-    </>
+    </AuthProvider>
   );
 };
+
+// import { Footer } from "./Footer";
+// import { Header } from "./Header";
+// import { Outlet, useNavigation } from "react-router";
+// import { ChatBtn } from "../ui/aiChatBtn";
+
+// export const AppLayout = () => {
+//   const navigation = useNavigation();
+//   const isLoading = navigation.state === "loading";
+
+//   return (
+//     <>
+//       <Header />
+//       <div className="min-h-dvh">
+//         {isLoading && (
+//           <div className="container">
+//             <span className="loading loading-ring loading-xs"></span>
+//             <span className="loading loading-ring loading-sm"></span>
+//             <span className="loading loading-ring loading-md"></span>
+//             <span className="loading loading-ring loading-lg"></span>
+//             <span className="loading loading-ring loading-xl"></span>
+//           </div>
+//         )}
+//         <Outlet />
+//       </div>
+//       <ChatBtn />
+//       <Footer />
+//     </>
+//   );
+// };

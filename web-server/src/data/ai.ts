8deb24b946aab39b70b5d9bb554f7f4011b28385
unknown <anitharaajanthira@gmail.com>
2026-03-18 12:44:51 +0100

    AI chat

--- web-server/src/data/ai.ts
+++ web-server/src/data/ai.ts
@@ -1,6 +1,5 @@
 const AI_SERVER_URL = import.meta.env.VITE_AI_SERVER_URL;
-if (!AI_SERVER_URL)
-  throw new Error("AI_SERVER_URL is required, are you missing a .env file?");
+if (!AI_SERVER_URL) throw new Error("AI_SERVER_URL is required, are you missing a .env file?");
 const baseURL = `${AI_SERVER_URL}/ai/chat`;
 
 type ChatBody = {
@@ -15,7 +14,7 @@ type ChatRes = {
 
 export const createChat = async (body: ChatBody): Promise<ChatRes> => {
   const accessToken = localStorage.getItem("accessToken"); // in case user is logged in, save token
-  const response = await fetch(`${baseURL}/chat`, {
+  const response = await fetch(`${baseURL}`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
